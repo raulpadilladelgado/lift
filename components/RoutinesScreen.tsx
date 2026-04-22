8d604f6d1fd6d9f43cf02a166355334adfe87e9c
Raúl Padilla <raul.padilla@leanmind.es>
2026-04-22 16:29:08 +0100

    fix: open visible alternative exercise from routine card

--- components/RoutinesScreen.tsx
+++ components/RoutinesScreen.tsx
@@ -381,7 +381,7 @@ export const RoutinesScreen: React.FC<Props> = ({
                     onUpdateForm={(field, value) => updateLogForm(displayExercise.id, field, value)}
                     onLog={() => handleLog(displayExercise.id)}
                     onLongPress={() => setActionSheetExerciseId(exercise.id)}
-                     onTap={() => onNavigateToExercise(exercise.id, activeRoutine.id)}
+                    onTap={() => onNavigateToExercise(displayExercise.id, activeRoutine.id)}
                     onToggleAlternative={() => setUsingAlternative((prev) => ({ ...prev, [exercise.id]: !prev[exercise.id] }))}
                   />
                 );
